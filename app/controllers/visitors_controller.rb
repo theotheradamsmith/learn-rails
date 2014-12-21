ae9fed77c950589fc71b31543021cb2adccd29d1
Adam Smith <hrotsvit@gmail.com>
2014-12-20 18:13:42 -0600

    Front-end framework

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -4,8 +4,6 @@ class VisitorsController < ApplicationController
   
   def new
     @owner = Owner.new
-    flash.now[:notice] = 'Welcome!'
-    flash.now[:alert] = 'My birthday is soon.'
   end
 
 end
