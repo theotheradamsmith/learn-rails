10ee1964b987b724776f35e39e2d456242acc502
Adam Smith <hrotsvit@gmail.com>
2014-12-24 11:44:31 -0600

    Integration test for home page added

---
+++ test/integration/home_page_test.rb
@@ -0,0 +1,15 @@
+# Feature: Home Page
+#   As a visitor
+#   I want to visit a home page
+#   So I can learn more about the website
+feature 'Home page' do
+
+  # Scenario: Visit the home page
+  #   Given I am a visitor
+  #   When I visit the home page
+  #   Then I see "Welcome"
+  scenario 'visit the home page' do
+    visit root_path
+    page.must_have_content 'Stay in touch'
+  end
+end
